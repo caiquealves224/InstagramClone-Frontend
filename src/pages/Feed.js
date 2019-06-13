023c29c03bb85ee7808dba12a6b2bc7f261e0710
Caique Alves <caiquealves224@gmail.com>
2019-06-12 23:23:08 -0300

    estilizando o post

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
+import "./Feed.css";
+
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 
 class Feed extends Component{
     render(h) {
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Caique Alves</span>
+                            <span className="place">GRU aka Guarulhos, SP</span>
+                        </div>
+
+                        <img src={more} alt="Mais"/>
+                    </header>
+
+                    <img src="http://localhost:3333/files/OmniStack%20-%20Wallpaper%20Mobile.jpg" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like}/>
+                            <img src={comment}/>
+                            <img src={send}/>
+                        </div>
+
+                        <strong>990</strong>
+
+                        <p>
+                            style style, Uhuu, react!!!
+
+                            <span>
+                                #VamoQueVamo
+                            </span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         )
     }
 }
